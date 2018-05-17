d9cee9a6d270a49625451691b04a1b3c2044a9a2
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-05-17 00:39:26 -0300

    :sparkles: Apply header

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -1,19 +1,61 @@
-import React, {Component} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import styled from 'styled-components'
 
-class App extends Component {
+import logo from './logo.png'
+
+const Wrapper = styled.div`
+  padding: 3px;
+  display: flex;
+  align-items: center;
+  background-color: #ff6503;
+`
+
+const Title = styled.span`
+  font-weight: bold;
+  margin-left: .5em;
+`
+
+const Logo = styled.img`
+  width: 18px;
+  height: 18px;
+  border: 1px solid white;
+`
+
+const Menu = styled.ul`
+  margin: 0;
+  display: flex;
+  list-style: none;
+  padding-left: .5em;
+  
+  li {
+    display: flex;
+    
+    &:not(:last-child):after {
+      content: '|';
+      display: block;
+      margin-left: .5em;
+      margin-right: .5em;
+    }
+  }
+`
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Logo src={logo}/>
+        <Title>Hacker News</Title>
+        <Menu>
+          <li>welcome</li>
+          <li>new</li>
+          <li>threads</li>
+          <li>comments</li>
+          <li>show</li>
+          <li>ask</li>
+          <li>jobs</li>
+          <li>submit</li>
+        </Menu>
+      </Wrapper>
     )
   }
 }
